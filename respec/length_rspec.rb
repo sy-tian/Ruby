1c4400cead04de96f4cb6f328ff8918ada001023
szetin <1911165211@qq.com>
2014-08-10 13:34:38 +0800

    extract the common variable

--- respec/length_rspec.rb
+++ respec/length_rspec.rb
@@ -1,18 +1,19 @@
 require '../src/length'
 describe Length do
+  before do
+    @length_one = Length.new 3
+  end
   it 'should_be_true_for_same_values' do
-    length_one = Length.new 3
-    length_two = Length.new 3
 
-    is_equal = length_one.is_equal(length_two)
+    length_two = Length.new 3
+    is_equal = @length_one.is_equal(length_two)
 
     expect(is_equal).to eq true
-    end
+  end
   it 'should_be_false_for_different_values' do
-    length_one = Length.new 3
-    length_two = Length.new 4
 
-    is_equal = length_one.is_equal(length_two)
+    length_two = Length.new 4
+    is_equal = @length_one.is_equal(length_two)
 
     expect(is_equal).to eq false
   end
